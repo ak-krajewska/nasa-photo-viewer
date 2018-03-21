5fdd73fb97ad48639b8373e98aafeb830a41b653
AK Krajewska <akrajewska@gmail.com>
2018-03-21 16:17:24 -0700

    minimal search funciton implemented

---
+++ resources/main.js
@@ -0,0 +1,82 @@
+//use destructuring on the json somehow, so it's less annoying to parse
+//handle 200, 400, and 500 codes using promises (then -- which I still don't understand)
+//use a mouseover event to make a caption appear
+//use a default value for when the user doesn't put anything in the search box
+//use template strings to display all text on the page
+//use arrow functions
+
+var response = null;
+var photos = null; 
+
+document.getElementById('search').addEventListener('click', function() {
+  console.log('you clicked search');
+    let keyword = document.getElementById('keyword').value;
+    searchPhotos(keyword);
+});
+
+function listThumbnails() {
+    // j.collection.items[1].data[0].media_type);
+   photos.forEach(function(element, index) {
+       if (photos[index].data[0].media_type === 'image'){
+            var li = document.createElement('li');
+            li.innerHTML = `<img src=${photos[index].links[0].href}>`;
+            document.getElementById('list').appendChild(li);
+       }
+  });
+}
+
+function clearPrevious() {
+    let node = document.getElementById('list');
+    while (node.hasChildNodes()) {
+    node.removeChild(node.lastChild);
+    }
+}
+
+function searchPhotos(keyword) {
+  fetch(`https://images-api.nasa.gov/search?q=${keyword}`)
+    .then(function(response) {
+      return response.json();
+    })
+    //do some error checking
+    .then(function(r) {
+      console.log(r);
+      response = r;
+      photos = response.collection.items;
+      console.log(photos);
+      //console.log(photos[0].links[0].href);
+    
+      clearPrevious();
+      listThumbnails();
+  })
+    /*
+    .then(function(j) {
+      response = j;
+      assignValues();
+      getFollowers(j.followers_url);
+    })*/
+    
+}
+
+// collection.metadata.total_hits = 0 means no results
+
+
+/*
+fetch('https://images-api.nasa.gov/search?q=mars')
+    //fetch('https://api.github.com/users/cassidoo')
+      .then(function(response) {
+        return response.json();
+      })
+
+.then(function(j) {
+        console.log(j);
+        console.log(j.collection.items[1]);
+     console.log("title: " + j.collection.items[1].data[0].title);
+     console.log("media type: " + j.collection.items[1].data[0].media_type);
+     console.log("link to json with all the image sizes: " + j.collection.items[1].href);
+    console.log("thumbnail :" + j.collection.items[1].links[0].href);
+
+      }); */
+
+//you'll have to check if the media type is image
+//might need to limit the search results OR do pagination somehow
+//you probably want to pull in the entire response and store it
